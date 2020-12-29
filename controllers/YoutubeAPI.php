f8b1ebd4d2566af88866c562833c023d2505709e
Cri <criamodn@fucker>
2020-12-29 23:15:18 +0300

    yaa

--- controllers/YoutubeAPI.php
+++ controllers/YoutubeAPI.php
@@ -1,10 +1,25 @@
 <?php
 namespace Superproject\YouTubeVideo;
 
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
 class YouTubeAPI{
     private $Apikey;
     function __construct()
     {}
 
 
+}
+
+
+use yii\httpclient\Client;
+
+$client = new Client();
+$response = $client->createRequest()
+    ->setMethod('POST')
+    ->setUrl('http://example.com/api/1.0/users')
+    ->setData(['name' => 'John Doe', 'email' => 'johndoe@example.com'])
+    ->send();
+if ($response->isOk) {
+    $newUserId = $response->data['id'];
 }
