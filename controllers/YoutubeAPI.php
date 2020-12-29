9f05e98b45051d45b5e2330b964f1a90ee4654f3
Cri <criamodn@fucker>
2020-12-30 01:43:40 +0300

    yaa

--- controllers/YoutubeAPI.php
+++ controllers/YoutubeAPI.php
@@ -1,25 +1,39 @@
 <?php
 namespace Superproject\YouTubeVideo;
 
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-
 class YouTubeAPI{
-    private $Apikey;
+    private $Apikey="AIzaSyDbn9T0KXiqXATEF9Nm5w9PL6WaNwymshM";
     function __construct()
     {}
 
+    function getVideoStat($idVideo='eXQATmoBSI4')
+    {
+        $json_result = json_decode(file_get_contents ("https://www.googleapis.com/youtube/v3/videos?id=$idVideo&key=$this->Apikey&fields=items(id,snippet(channelId,title,categoryId,description,thumbnails),statistics)&part=snippet,statistics"));
+        //var_dump($json_result);
+        return $json_result->items[0];
 
+    }
+
+    function getChannelStat($idChannel='UCFPC7r3tWWXWzUIROLx46mg')
+    {
+        $json_result = json_decode(file_get_contents ("https://youtube.googleapis.com/youtube/v3/channels?part=statistics&part=snippet&id=$idChannel&key=$this->Apikey"));
+        //var_dump($json_result);
+        return $json_result->items[0];
+    }
 }
 
+$vid=new YouTubeAPI();
+$st=$vid->getVideoStat('eXQATmoBSI4');
 
-use yii\httpclient\Client;
 
-$client = new Client();
-$response = $client->createRequest()
-    ->setMethod('POST')
-    ->setUrl('http://example.com/api/1.0/users')
-    ->setData(['name' => 'John Doe', 'email' => 'johndoe@example.com'])
-    ->send();
-if ($response->isOk) {
-    $newUserId = $response->data['id'];
-}
+$ch=$vid->getChannelStat();
+
+$channelTitle=$ch->snippet->title;
+$channelcount=$ch->statistics->subscriberCount;
+
+
+$r=0;
+
+
+
+
--- web/index.php
+++ web/index.php
@@ -6,18 +6,5 @@
     $config = require __DIR__ . '/../config/web.php';
     $yii = new yii\web\Application($config);
 
-    use yii\httpclient\Client;
-
-    $client = new Client();
-    $response = $client->createRequest()
-        ->setMethod('POST')
-        ->setUrl('http://youtube.com/')
-        ->setData(['name' => 'John Doe', 'email' => 'johndoe@example.com'])
-        ->send();
-    if ($response->isOk) {
-        $newUserId = $response->data['id'];
-    }
-
-
     $yii->run ();
 
