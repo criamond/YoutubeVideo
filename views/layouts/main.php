21ce77956fbecd1d512614980a0d6a80dcc14a18
Cri <criamodn@fucker>
2021-01-05 17:07:39 +0300

    yaa

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,4 +1,5 @@
-<?php use yii\bootstrap\Nav;
+<?php
+use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar as NavBarAlias;
 
 $this->beginPage(); ?>
@@ -19,9 +20,17 @@ $this->beginPage(); ?>
                     'class' => 'navbar-default navbar-fixed-top'
             ]
         ]);
+        $items= [
+            ['label' => 'Video1', 'url'=> ['/#video1']],
+            ['label' => 'Video2', 'url'=> ['/#video2']],
+            ['label' => 'Video3', 'url'=> ['/#video3']],
+            ['label' => 'Video4', 'url'=> ['/#video4']],
+            ['label' => 'Video5', 'url'=> ['/#video5']]
+        ];
 
         echo Nav::widget([
-                'options'=>['class'=>'navbar-nav navbar-right']
+                'options'=>['class'=>'navbar-nav navbar-right'],
+                             'items'=> $items
 
         ]);
         NavBarAlias::end();
--- views/site/index.php
+++ views/site/index.php
@@ -1,47 +1,64 @@
-
-
 <?php
+
 use miloschuman\highcharts\Highcharts;
-use yii\web\JsExpression;
 
-for($i=0;$i<count($videos_array);$i++){
-    echo $videos_array[$i]["VideoName"];
-    for($j=0;$j<count($VideoStat[$i]);$j++){
-        $ViewsCountArray[$j]=(int) $VideoStat[$i][$j]['ViewsCount'];
-        $LikesArray[$j]=(int) $VideoStat[$i][$j]['Likes'];
-        $DislikesArray[$j]=(int) $VideoStat[$i][$j]['Dislikes'];
-        $CommentsCountArray[$j]=(int) $VideoStat[$i][$j]['CommentsCount'];
-        $SubscribersCountArray[$j]=(int) $VideoStat[$i][$j]['SubscribersCount'];
-        $DateTimeCategories[$j]=$VideoStat[$i][$j]['DateTime'];
-    }
-    echo Highcharts::widget([
-                                'options' => [
-                                    'title' => ['text' => 'Video Statistics '],
-                                    'xAxis' => array(
-                                        'type' => 'datetime',
-                                        //'dateTimeLabelFormats' => array('day' => '%e of %b'),
-                                        'categories' => $DateTimeCategories
-                                    ),
-                                    'yAxis' => [
+for ($i = 0; $i < count($videos_array); $i++) {
+    ?>
+    <a name="video<?= $i + 1 ?>">
+        <div class="row">
+            <div class="col-lg-2 col-md-2 col-sm-12">
+                <img align="center" src="<?= $videos_array[$i]["thumb"] ?>" class="img-fluid">
+            </div>
 
-                                        'title' => ['text' => '']
-                                    ],
-                                   // 'plotOptions' => ['series' => ['pointInterval'=> 3600 * 1000],'pointStart'=> 'Date.UTC(2010, 10, 11)'],
-                                    'series' => [
-                                        ['name' => 'Views', 'data' => $ViewsCountArray] ,
-                                        ['name' => 'Likes', 'data' => $LikesArray],
-                                        ['name' => 'Dislikes', 'data' => $DislikesArray],
-                                        ['name' => 'CommentsCount', 'data' => $CommentsCountArray],
-                                        ['name' => 'SubscribersCount', 'data' => $SubscribersCountArray]
+            <div class="col-lg-10 col-md-10 col-sm-12 desc">
+                <?php
+                echo "<h3> {$videos_array[$i]["VideoName"]} </h3>";
+                echo "<h3>ChannelName: {$videos_array[$i]["ChannelName"]} </h3>";
+                echo " {$videos_array[$i]["VideoDescription"]} ";
+                ?>
+            </div>
+        </div>
 
-                                    ]
-                                ]
-                            ]);
+        <?php
+        for ($j = 0; $j < count($VideoStat[$i]); $j++) {
+            $ViewsCountArray[$j] = (int)$VideoStat[$i][$j]['ViewsCount'];
+            $LikesArray[$j] = (int)$VideoStat[$i][$j]['Likes'];
+            $DislikesArray[$j] = (int)$VideoStat[$i][$j]['Dislikes'];
+            $CommentsCountArray[$j] = (int)$VideoStat[$i][$j]['CommentsCount'];
+            $SubscribersCountArray[$j] = (int)$VideoStat[$i][$j]['SubscribersCount'];
+            $DateTimeCategories[$j] = $VideoStat[$i][$j]['DateTime'];
+        }
+        echo Highcharts::widget(
+            [
+                'options' => [
+                    'title' => ['text' => 'Video Statistics '],
+                    'xAxis' => array(
+                        'type' => 'datetime',
+                        //'dateTimeLabelFormats' => array('day' => '%e of %b'),
+                        'categories' => $DateTimeCategories
+                    ),
+                    'yAxis' => [
 
+                        'title' => ['text' => '']
+                    ],
+                    // 'plotOptions' => ['series' => ['pointInterval'=> 3600 * 1000],'pointStart'=> 'Date.UTC(2010, 10, 11)'],
+                    'series' => [
+                        ['name' => 'Views', 'data' => $ViewsCountArray],
+                        ['name' => 'Likes', 'data' => $LikesArray],
+                        ['name' => 'Dislikes', 'data' => $DislikesArray],
+                        ['name' => 'CommentsCount', 'data' => $CommentsCountArray],
+                        ['name' => 'SubscribersCount', 'data' => $SubscribersCountArray]
+
+                    ]
+                ]
+            ]
+        );
+
+        ?>
+        <br><br><br>
+    </a>
+    <?php
 }
-
-
-
 
 
 ?>
